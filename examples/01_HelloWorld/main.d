9064c90832ca98d383f04643d5f1fd2cb6516e32
NCrashed <NCrashed@gmail.com>
2013-07-22 20:22:56 +0400

    Removed ponters from example

--- examples/01_HelloWorld/main.d
+++ examples/01_HelloWorld/main.d
@@ -6,7 +6,7 @@ import irrlicht.gui;
 
 int main()
 {
-	IrrlichtDevice *device = 
+	IrrlichtDevice device = 
 		createDevice(Renderer.EDT_SOFTWARE, dimension2d!uint(640, 480),
 			false, false, false, 0);
 
@@ -15,20 +15,20 @@ int main()
 
 	device.setWindowCaption("Hello World! - Irrlicht Engine Demo");
 
-	IVideoDriver* driver = device.getVideoDriver();
-	ISceneManager* smgr = device.getSceneManager();
-	IGUIEnvironment* guienv = device.getGUIEnvironment();
+	IVideoDriver driver = device.getVideoDriver();
+	ISceneManager smgr = device.getSceneManager();
+	IGUIEnvironment guienv = device.getGUIEnvironment();
 
 	guienv.addStaticText("Hello World! This is the Irrlicht Software renderer!",
 		rect!int(10,10,260,22), true);
 
-	IAnimatedMesh* mesh = smgr.getMesh("../../media/sydney.md2");
+	IAnimatedMesh mesh = smgr.getMesh("../../media/sydney.md2");
 	if(mesh is null)
 	{
 		device.drop();
 		return 1;
 	}
-	IAnimatedMeshSceneNode* node = smgr.addAnimatedMeshSceneNode(mesh);
+	IAnimatedMeshSceneNode node = smgr.addAnimatedMeshSceneNode(mesh);
 
 	if(node)
 	{
